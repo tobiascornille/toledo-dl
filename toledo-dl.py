9737cfda8fff82f9a0cf3ce279dccc79b6554466
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2022-03-05 11:31:56 +0100

    feat: regex, script cleanup

--- toledo-dl.py
+++ toledo-dl.py
@@ -9,60 +9,60 @@ import sys
 import os
 
 
-def dl_url(url):
-  # List of (p, entry_id) tuples
-  videos = []
+def dl_url(toledo_url):
+    # List of (p, entry_id) tuples
+    videos = []
 
-  # Scrape video information from Toledo page
-  r = requests.get(url, cookies=cj)
-  html_content = r.text
-  for line in iter(html_content.splitlines()):
-    if '<iframe name="KalturaIframe"' in line:
-      pattern = '/p/(.+)/sp/.*/entry_id/(.*)/version'
-      regex_res = re.search(pattern, line)
-      try:
-        kaltura_p = regex_res.group(1)
-        kaltura_entry_id = regex_res.group(2)
-        videos.append((kaltura_p, kaltura_entry_id))
-      except:
-        print("Regex matching failed on: " + line)
+    # Scrape video information from Toledo page
+    # r = requests.get(url, cookies=cj)
+    html_content = page
+    for line in iter(html_content.splitlines()):
+        if '<iframe name="KalturaIframe"' in line:
+            pattern = '/p/(.+?)/sp/.*?/entry_id/(.*?)/version'
+            regex_res = re.search(pattern, line)
+            try:
+                kaltura_p = regex_res.group(1)
+                kaltura_entry_id = regex_res.group(2)
+                videos.append((kaltura_p, kaltura_entry_id))
+            except:
+                print("Regex matching failed on: " + line)
 
-  print('Found {} videos'.format(len(videos)))
+    print('Found {} videos'.format(len(videos)))
 
-  # Make directory for videos
-  dir_name = 'toledo-dl-{}'.format(int(hashlib.sha512(url.encode()).hexdigest(), 16) % 262144)
-  dir_path = pathlib.Path(dir_name)
-  dir_path.mkdir(exist_ok=True)
-  # For some reason cd command doesn't work on Windows. Took it out of the subprocess call.
-  os.chdir(dir_path)
+    # Make directory for videos
+    dir_name = 'toledo-dl-{}'.format(int(hashlib.sha512(toledo_url.encode()).hexdigest(), 16) % 262144)
+    dir_path = pathlib.Path(dir_name)
+    dir_path.mkdir(exist_ok=True)
+    # For some reason cd command doesn't work on Windows. Took it out of the subprocess call.
+    os.chdir(dir_path)
 
-  # Download videos using youtube-dl
-  for idx, video in enumerate(videos):
-    print('Downloading video {}/{}'.format(idx + 1, len(videos)))
-    subprocess.run('youtube-dl -f "[protocol=m3u8_native]" kaltura:{}:{}'.format(video[0], video[1]), shell=True)
-  # Change back to parent folder after downloading all videos in list.
-  os.chdir('..')
+    # Download videos using youtube-dl
+    for idx, video in enumerate(videos):
+        print('Downloading video {}/{}'.format(idx + 1, len(videos)))
+        subprocess.run('youtube-dl -f "[protocol=m3u8_native]" kaltura:{}:{}'.format(video[0], video[1]), shell=True)
+    # Change back to parent folder after downloading all videos in list.
+    os.chdir('..')
 
-  # # Shortens videos
-  # for path in dir_path.glob('*'):
-  #     video_path = str(path)
-  #     if not video_path.endswith('_ALTERED.mp4') and not pathlib.Path('{}_ALTERED.mp4'.format(video_path[:-4])).is_file():
-  #       print('Shortening video {}'.format(video_path))
-  #       subprocess.run('cd jumpcutter; python3 jumpcutter.py --input_file "../{}" -snd 1 -sil 20 -fm 6'.format(video_path), shell=True)
+    # # Shortens videos for path in dir_path.glob('*'): video_path = str(path) if not video_path.endswith(
+    # '_ALTERED.mp4') and not pathlib.Path('{}_ALTERED.mp4'.format(video_path[:-4])).is_file(): print('Shortening
+    # video {}'.format(video_path)) subprocess.run('cd jumpcutter; python3 jumpcutter.py --input_file "../{}" -snd 1
+    # -sil 20 -fm 6'.format(video_path), shell=True)
 
 
 # Cookies
 # Fix cookies.txt
 with open('cookies.txt', 'r') as original: data = original.read()
+with open('page.txt', 'r', encoding="utf-8") as owo: page = owo.read()
 # Fix 0 expiry time, see https://stackoverflow.com/questions/14742899/using-cookies-txt-file-with-python-requests
 expiry_time = round(time.time() + 14 * 24 * 3600)
 data = data.replace('\t0\t', '\t{}\t'.format(expiry_time))
-with open('cookies.txt', 'w') as modified: 
-  if not data.startswith('# Netscape'):
-    # Add correct first line, see https://stackoverflow.com/questions/14742899/using-cookies-txt-file-with-python-requests
-    modified.write('# Netscape HTTP Cookie File\n' + data)
-  else:
-    modified.write(data)
+with open('cookies.txt', 'w') as modified:
+    if not data.startswith('# Netscape'):
+        # Add correct first line, see https://stackoverflow.com/questions/14742899/using-cookies-txt-file-with-python
+        # -requests
+        modified.write('# Netscape HTTP Cookie File\n' + data)
+    else:
+        modified.write(data)
 # Load cookies from cookies.txt
 cj = MozillaCookieJar('cookies.txt')
 cj.load(ignore_discard=True)
@@ -71,4 +71,4 @@ print('Read {} cookies from cookies.txt'.format(len(cj)))
 # Reading input file
 input_file = open(sys.argv[1], 'r')
 for url in input_file.readlines():
-  dl_url(url)
+    dl_url(url)
